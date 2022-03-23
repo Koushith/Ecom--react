9f0529b4e30992805c4cea9a4b15322be4db2ba0
koushith <koushith97@gmail.com>
2022-03-24 01:27:57 +0530

    adds wishlist page

--- src/components/product-card/product-card.component.js
+++ src/components/product-card/product-card.component.js
@@ -2,29 +2,9 @@ import React from 'react';
 import { Button } from '../primitives/button/button.component';
 import productCardStyles from './product-card.module.css';
 
-function ProductCard() {
+function ProductCard(props) {
+  const { buttonLabel } = props;
   return (
-    // <div class='card ecomm'>
-    //   <div class='product-image'>
-    //     <img
-    //       src='https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/productimage/2021/2/18/07b7a314-decd-421a-b447-01b2411e24e61613643428870-1.jpg'
-    //       alt='card-pic'
-    //     />
-    //   </div>
-    //   <div class='card-badge'>NEW</div>
-    //   <h2 class='card-title'>Athena top</h2>
-    //   <div class='card-text'>4 star</div>
-    //   <div class='card-price'>
-    //     <span class='price-now'> Rs.764 </span>
-    //     <span class='discount'> (62% OFF) </span>
-    //   </div>
-
-    //   <div style={{ display: 'flex', background: 'red' }}>
-    //     <button class='btn btn-primary '>ADD TO CART</button>
-
-    //     <i className='fa-solid fa-mobile'></i>
-    //   </div>
-    // </div>
     <div className={productCardStyles.ecommerce_card}>
       <div className={productCardStyles.product_image}>
         <img
@@ -48,7 +28,7 @@ function ProductCard() {
         </div>
 
         <div className={productCardStyles.actions_button}>
-          <Button label='ADD TO CART' variant='primary' />
+          <Button label={buttonLabel} variant='primary' />
           <i className='fa-regular fa-heart'></i>
           {/* <i className='fa-solid fa-heart'></i> */}
         </div>
--- src/screens/wishlist/wishlist.screen.js
+++ src/screens/wishlist/wishlist.screen.js
@@ -1,3 +1,22 @@
+import { Heading } from '../../components/primitives/text/text.component';
+import ProductCard from '../../components/product-card/product-card.component';
+import wishlistScreen from './wishlist.screen.module.css';
+
 export const WishListScreen = () => {
-  return <>WishhList Page</>;
+  return (
+    <section className={wishlistScreen.wishlist_container}>
+      <div className={wishlistScreen.heading}>
+        <Heading label='My Cart (1)' />
+      </div>
+
+      <div className={wishlistScreen.product_container}>
+        <ProductCard buttonLabel='MOVE TO CART' />
+        <ProductCard buttonLabel='MOVE TO CART' />
+        <ProductCard buttonLabel='MOVE TO CART' />
+        <ProductCard buttonLabel='MOVE TO CART' />
+        <ProductCard buttonLabel='MOVE TO CART' />
+        <ProductCard buttonLabel='MOVE TO CART' />
+      </div>
+    </section>
+  );
 };
